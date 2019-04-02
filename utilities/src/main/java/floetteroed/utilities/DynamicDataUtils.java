be76e8cccfb03cbcdbd1803d507f25dea2fbd022
GunnarF <GunnarF@GUNNARF-830G5.vti.se>
2019-04-02 07:47:29 +0200

    added two classes

---
+++ utilities/src/main/java/floetteroed/utilities/DynamicDataUtils.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018 Gunnar Flötteröd
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * contact: gunnar.flotterod@gmail.com
+ *
+ */
+package floetteroed.utilities;
+
+/**
+ *
+ * @author Gunnar Flötteröd
+ *
+ */
+public class DynamicDataUtils {
+
+	private DynamicDataUtils() {
+	}
+	
+	public static <L> double sumOfEntries2(final DynamicData<L> data) {
+		double result = 0.0;
+		for (L locObj : data.keySet()) {
+			for (int bin = 0; bin < data.getBinCnt(); bin++) {
+				final double val = data.getBinValue(locObj, bin);
+				result += val * val;
+			}
+		}
+		return result;
+	}
+
+	public static <L> double sumOfDifferences2(final DynamicData<L> counts1, final DynamicData<L> counts2) {
+		if (counts1.getBinCnt() != counts2.getBinCnt()) {
+			throw new RuntimeException(
+					"arg1 has " + counts1.getBinCnt() + " bins, but arg2 has " + counts2.getBinCnt() + " bins.");
+		}
+		double result = 0.0;
+		for (L locObj : SetUtils.union(counts1.keySet(), counts2.keySet())) {
+			for (int bin = 0; bin < counts1.getBinCnt(); bin++) {
+				final double diff = counts1.getBinValue(locObj, bin) - counts2.getBinValue(locObj, bin);
+				result += diff * diff;
+			}
+		}
+		return result;
+	}
+
+	public static <L> DynamicData<L> newDifference(final DynamicData<L> data1, final DynamicData<L> data2,
+			final double weight) {
+		if (data1.getBinCnt() != data2.getBinCnt()) {
+			throw new RuntimeException(
+					"arg1 has " + data1.getBinCnt() + " bins, but arg2 has " + data2.getBinCnt() + " bins.");
+		}
+		final DynamicData<L> result = new DynamicData<L>(data1.getStartTime_s(), data1.getBinSize_s(),
+				data1.getBinCnt());
+		for (L locObj : SetUtils.union(data1.keySet(), data2.keySet())) {
+			for (int bin = 0; bin < data1.getBinCnt(); bin++) {
+				result.put(locObj, bin, weight * (data1.getBinValue(locObj, bin) - data2.getBinValue(locObj, bin)));
+			}
+		}
+		return result;
+	}
+}
+
---
+++ utilities/src/main/java/floetteroed/utilities/SetUtils.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 Gunnar Flötteröd
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * contact: gunnar.flotterod@gmail.com
+ *
+ */
+package floetteroed.utilities;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author Gunnar Flötteröd
+ *
+ *         TODO Move this to the utilities-project.
+ * 
+ */
+public class SetUtils {
+
+	public static <T> Set<T> union(final Set<? extends T> arg0, final Set<? extends T> arg1) {
+		final Set<T> result = new LinkedHashSet<>(arg0);
+		result.addAll(arg1);
+		return result;
+	}
+
+	public static <T> Set<T> intersect(final Set<? extends T> arg0, final Set<? extends T> arg1) {
+		final Set<T> result = new LinkedHashSet<>(arg0);
+		result.retainAll(arg1);
+		return result;
+	}
+}
