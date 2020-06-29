7e06fd275a64cc895935fc19e430d72f20aba7e4
CLIVTI <chengxi@abe.kth.se>
2020-06-29 13:54:56 +0200

    test commit from chengxi

---
+++ experimental/src/main/java/stockholm/chengxi/bicyclematsim/transcadnetworkimport/Test.java
@@ -0,0 +1,11 @@
+package stockholm.chengxi.bicyclematsim.transcadnetworkimport;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test done.");
+
+	}
+
+}
---
+++ experimental/src/main/java/stockholm/chengxi/bicyclematsim/transcadnetworkimport/package-info.java
@@ -0,0 +1,1 @@
+package stockholm.chengxi.bicyclematsim.transcadnetworkimport;
